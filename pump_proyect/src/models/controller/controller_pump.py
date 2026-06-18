f8c2fd7384c14ddea078d8454ccb26b8d50de20d
Francisco Natale <francisco_natale@hotmail.com>
2026-06-18 18:20:38 -0300

    add time advance and partial ext transition

--- pump_proyect/src/models/controller/controller_pump.py
+++ pump_proyect/src/models/controller/controller_pump.py
@@ -1,13 +1,22 @@
 from enum import Enum
-from flow_state import FlowState, FlowStatus
-from bag_state import BagState, FlowStatus
+from typing import Tuple, List
+
+class FlowState(Enum):
+    NORMAL_FLOW = 1
+    MEDIUM_FLOW = 2
+    CRITICAL_FLOW = 3
+
+class BagState(Enum):
+    NORMAL_BAG = 1
+    END_BAG = 2
+    EMPTY_BAG = 3
+
+Action = Tuple[str, float]  
 
 class PumpController(AtomicDEVS):
-
-    def __init__(self, max_tolerance_exceedance_seconds=5):
+    def __init__(self):
         super().__init__("PumpController")
-        self.max_tolerance_exceedance_seconds = max_tolerance_exceedance_seconds
-
+        self.max_tolerance_exceedance_seconds = 5
         self.margin_tolerance =  0.1
         self.in_medical_order = self.addInPort("in_medical_order")
         self.in_end_bag = self.addInPort("in_end_bag")
@@ -17,14 +26,36 @@ class PumpController(AtomicDEVS):
         self.out_turn_off_bomb = self.addOutPort("out_turn_off_bomb")
         self.out_alarm = self.addOutPort("out_alarm")
         self.out_log = self.addOutPort("out_log")
-        self.state = {FlowState: FlowState(NORMAL_STATUS), BagState: BagState(NORMAL_BAG, float('inf')), "exceeded_tolerance_count": 0, last_sensor_medition: 0, objetive: 0, actions: []}
+        self.state = {
+            "flow_state": (FlowState.NORMAL_FLOW, 0.0),
+            "bag_state": (BagState.NORMAL_BAG, float('inf')),
+            "last_sensor_medition": 0.0,
+            "medical_order": 0.0,
+            "tolerance_exceedance_start_time": 0.0,
+            "actions": []  
+        }
+    
+    def add_action(self, action: str, time: float) -> None:
+        """Agregar acción (acción, tiempo) a la cola"""
+        self.state["actions"].append((action, time))
+    
+    def get_next_action(self) -> Action:
+        """Obtener siguiente acción"""
+        action, time = self.state["actions"].pop(0)
+        return action, time
 
     def tolerance_exceeded(self, last_sensor_medition, objetive):
         return (abs(last_sensor_medition - objetive)) / objetive > self.margin_tolerance
     
-    def intTransition(self):
-        
+    def timeAdvance(self):
+        if(not self.state["actions"].isEmpty()):
+            return get_next_action()[1]
+        else if(self.state["actions"].isEmpty() and self.state["bag_state"].awaiting_stop_bag()):
+            return self.state["bag_state"].get_time_to_empty()
+        else if(self.state["actions"].isEmpty() and not self.state["bag_state"].awaiting_stop_bag()):
+            return float('inf')
 
-
-
+    def extTransition(self, inputs):
+        if(self.state["medical_order"] == 0.0):
+            return {}
     
--- pump_proyect/src/models/controller/states/bag_state.py
+++ pump_proyect/src/models/controller/states/bag_state.py
@@ -4,13 +4,21 @@ class BagStatus(Enum):
     NORMAL_BAG = "normal_bag"
     END_BAG = "end_bag"
     EMPTY_BAG = "empty_bag"
+    AWAIT_STOP_BAG = "await_stop_bag"
 
 class BagState:
     def __init__(self, status=BagStatus.NORMAL_BAG, time_to_empty_bag=float('inf')):
         self.status = status
         self.time_to_empty_bag = time_to_empty_bag
+
     def change_status(self, new_status):
         self.status = new_status
 
     def update_time_to_empty(self, time):
-        self.time_to_empty_bag = time 
+        self.time_to_empty_bag = time  
+    
+    def get_time_to_empty(self):
+        return self.time_to_empty_bag
+
+    def get_status(self):
+        return self.status
